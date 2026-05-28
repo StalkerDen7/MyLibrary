4b80e9c1d0adeae38861f6d77f304d1b6e27f752
leon <Stalkerdenleon@gmail.com>
2026-05-28 10:44:55 +0300

    create pydantic schemas

---
+++ schemas/books.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class SBookBase(BaseModel):
+    title: str
+    author: str
+    year: int 
+    pages: int = Field(..., gt=10, description="Number of pages must be greater than 10")
+    is_read: bool = False
+
+class SBookAdd(SBookBase):
+    pass
+
+class SBook(SBookBase):
+    id: int
+
+    model_config = ConfigDict(from_attributes=True)
